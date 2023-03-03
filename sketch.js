4683a64a867a98eebadd611ec586e193deb0c147
ishaag2402 <ishaag2402@gmail.com>
2023-03-03 18:31:52 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+//   rect(100, 100, 100, 100);
+
+//   var controller = Leap.loop({enableGestures:true}, function(frame){
+//     var currentFrame = frame;
+//     var previousFrame = controller.frame(1);
+//     var tenFramesBack = controller.frame(10);
+//   });
+
+//   controller.connect();
+
+// controller.on('frame', onFrame);
+// function onFrame(frame) {
+//   console.log("Frame event for frame " + frame.id);
+// }
+
+
+
+  // console.log(test);
+
+  // var frameString = "Frame ID: " + frame.id  + "<br />"
+  //               + "Timestamp: " + frame.timestamp + " &micro;s<br />"
+  //               + "Hands: " + frame.hands.length + "<br />"
+  //               + "Fingers: " + frame.fingers.length + "<br />";
+                
+}
+
